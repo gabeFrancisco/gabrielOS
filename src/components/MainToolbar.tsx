bcf4489e035e1602bd21685d487006dc8326a20f
gabeFrancisco <gabegit2@gmail.com>
2025-06-22 14:11:07 -0300

    added W95FA font

--- src/components/MainToolbar.tsx
+++ src/components/MainToolbar.tsx
@@ -1,8 +1,8 @@
-import G from "../../public/favicon.png"
+import Computer from "../../public/icons/desktop.ico"
 
 function MainToolbar() {
   return (<div className="w-screen flex p-1 flex-row justify-between border-t border-2 border-gray-400 bg-gray-200 h-10">
-      <img src={G} alt="Menu Icon" className="w-7" />
+      <img src={Computer} alt="Menu Icon" className="w-7" />
   </div>);
 }
 
