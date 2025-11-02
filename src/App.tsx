8c01c349154f78d0e38b14eb798b6b9f2ff1102d
gabeFrancisco <gabegit2@gmail.com>
2025-11-02 18:47:27 -0300

    fixed menu bar garbage code

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import { useState } from 'react'
 import './App.css'
 import Desktop from './components/Desktop/Desktop'
 import MainToolbar from './components/MainToolbar/MainToolbar'
 
 function App() {
-  const [menu, setMenu] = useState(false);
-  const handleMenu = () => {
-    setMenu(!menu)
-    console.log(menu)
-  }
-
   return (
     <main className='w-screen flex flex-col justify-between h-screen bg-blue-950'>
-      <Desktop menu={menu}/>
+      <Desktop/>
       {/* <Warning/> */}
-      <MainToolbar handleMenuBar={handleMenu}/>
+      <MainToolbar/>
     </main>
   )
 }
