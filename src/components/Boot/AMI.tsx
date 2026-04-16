826f33775d71234da87155c4a215ee14964cc20b
gabefrancisco <gabegit2@gmail.com>
2026-04-16 17:09:24 -0300

    changed AMI image InternetProgram window

--- src/components/Boot/AMI.tsx
+++ src/components/Boot/AMI.tsx
@@ -1,8 +1,8 @@
-import AmiScreen from '../../../public/ami.png'
+import AmiScreen from '../../../public/ami.jpg'
 
 function Ami() {
   return (
-    <img src={AmiScreen} className='w-screen h-screen' alt='Ami'/>
+    <img src={AmiScreen} className='w-screen h-screen' alt='Ami' />
   );
 }
 
--- src/components/Programs/InternetProgram.tsx
+++ src/components/Programs/InternetProgram.tsx
@@ -1,7 +1,21 @@
-function InternetProgram() {
-    return (<div>
+import useSystemStore from "../../hooks/useSystemStore";
+import Window from "../Windows/Window";
 
-    </div>);
+interface Props {
+    position: { x: number, y: number }
+}
+
+function InternetProgram(props: Props) {
+
+    const store = useSystemStore();
+
+    return (<Window title="Browser" id="web" onClose={() => store.setOpen('web', false)} position={props.position}>
+        <div>
+            <iframe className="w-screen h-96" src="https://web.archive.org/web/20001019044252/http://www.counter-strike.net/">
+
+            </iframe>
+        </div>
+    </Window>);
 }
 
 export default InternetProgram;
