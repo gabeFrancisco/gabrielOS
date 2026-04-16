19980a38b06c356abf4ef9f2fc3efc0c81e15f8e
gabefrancisco <gabegit2@gmail.com>
2026-04-16 16:51:30 -0300

    created useDoubeTap hook

--- src/components/Desktop/Icon.tsx
+++ src/components/Desktop/Icon.tsx
@@ -1,12 +1,15 @@
-interface Props{
+import { useDoubleTap } from "../../hooks/useDoubleTap";
+
+interface Props {
   icon: string,
   label: string,
-  doubleClick: React.MouseEventHandler
+  doubleClick: () => void
 }
 
 function Icon(props: Props) {
+  const handleDoubleTap = useDoubleTap(props.doubleClick);
   return (
-    <div onDoubleClick={props.doubleClick} className="m-5 p-1 flex flex-col justify-center items-center cursor-pointer hover:bg-sky-700 backdrop-opacity-70">
+    <div onDoubleClick={props.doubleClick} onTouchEnd={handleDoubleTap} className="m-5 p-1 flex flex-col justify-center items-center cursor-pointer hover:bg-sky-700 backdrop-opacity-70">
       <img src={props.icon} alt="Resume" className="w-9" />
       <p className="text-white text-sm mt-1">{props.label}</p>
     </div >);
